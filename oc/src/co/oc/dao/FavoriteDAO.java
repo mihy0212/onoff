c37c4cb67e35742145db32eb0f88378112ea239a
mihy0212 <choimhyeon01@naver.com>
2019-10-04 10:11:47 +0900

    asdf

--- oc/src/co/oc/dao/FavoriteDAO.java
+++ oc/src/co/oc/dao/FavoriteDAO.java
@@ -1,5 +1,12 @@
 package co.oc.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import co.oc.dto.FavoriteDTO;
+
 public class FavoriteDAO extends DAO {
 	
 	private static FavoriteDAO instance = new FavoriteDAO();
@@ -7,6 +14,28 @@ public class FavoriteDAO extends DAO {
 		return instance;
 	}
 	
-	
+	//전체 회원 즐겨찾기 조회
+	public List<FavoriteDTO> selectAll(Connection conn){
+		List<FavoriteDTO> list = new ArrayList<FavoriteDTO>();
+		String sql = "select f.favo_num, f.user_num, f.store_num, s.store_name, s.store_categ1, s.store_categ2, s.store_categ3 from oc_favorite f join oc_store s on (f.store_sum = s.store_num) order by 1";
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while(rs.next()) {
+				FavoriteDTO dto = new FavoriteDTO();
+				dto.setFavoNum(rs.getString("favo_num"));
+				dto.setUserNum(rs.getString("user_num"));
+				dto.setStoreNum(rs.getString("store_num"));
+				dto.setStoreName(rs.getString("store_name"));
+				dto.setStoreCateg1(rs.getString("store_categ1"));
+				dto.setStoreCateg2(rs.getString("store_categ2"));
+				dto.setStoreCateg3(rs.getString("store_categ3"));
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 	
 }
--- oc/src/co/oc/dto/FavoriteDTO.java
+++ oc/src/co/oc/dto/FavoriteDTO.java
@@ -5,6 +5,10 @@ public class FavoriteDTO {
 	private String favoNum;
 	private String userNum;
 	private String storeNum;
+	private String storeName;
+	private String storeCateg1;
+	private String storeCateg2;
+	private String storeCateg3;
 	
 	
 	public String getFavoNum() {
@@ -25,4 +29,29 @@ public class FavoriteDTO {
 	public void setStoreNum(String storeNum) {
 		this.storeNum = storeNum;
 	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreCateg1() {
+		return storeCateg1;
+	}
+	public void setStoreCateg1(String storeCateg1) {
+		this.storeCateg1 = storeCateg1;
+	}
+	public String getStoreCateg2() {
+		return storeCateg2;
+	}
+	public void setStoreCateg2(String storeCateg2) {
+		this.storeCateg2 = storeCateg2;
+	}
+	public String getStoreCateg3() {
+		return storeCateg3;
+	}
+	public void setStoreCateg3(String storeCateg3) {
+		this.storeCateg3 = storeCateg3;
+	}
+	
 }
