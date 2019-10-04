ee6767ef9f159dd9531a4ab1da101ff773d95a6f
mihy0212 <choimhyeon01@naver.com>
2019-10-04 11:56:18 +0900

    다시 reviewDAO

--- oc/src/co/oc/dao/ReviewDAO.java
+++ oc/src/co/oc/dao/ReviewDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.Statement;
 import co.oc.dto.ReviewDTO;
 
 public class ReviewDAO extends DAO {
