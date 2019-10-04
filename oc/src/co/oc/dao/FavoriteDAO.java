1c5592569af168a3d46534499ab8ebf407f2d3c5
mihy0212 <choimhyeon01@naver.com>
2019-10-04 09:54:00 +0900

    asdf

---
+++ oc/src/co/oc/dao/FavoriteDAO.java
@@ -0,0 +1,12 @@
+package co.oc.dao;
+
+public class FavoriteDAO extends DAO {
+	
+	private static FavoriteDAO instance = new FavoriteDAO();
+	public static FavoriteDAO getInstance() {
+		return instance;
+	}
+	
+	
+	
+}
